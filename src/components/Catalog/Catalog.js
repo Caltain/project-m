ffc4d63408e3f4cfd70ddb6ca09683de95c9e5bc
Caltain <Caltain@abv.bg>
2021-12-02 17:56:53 +0200

    some css changes

---
+++ src/components/Catalog/Catalog.js
@@ -0,0 +1,21 @@
+
+
+
+const Catalog = () => {
+    return (
+        <section id="Catalog-page" className="Catalog">
+       
+
+            <section>
+                
+                    <h1>Furniture</h1>
+              
+                
+            </section>
+
+        </section>
+        
+    );
+}
+
+export default Catalog;
