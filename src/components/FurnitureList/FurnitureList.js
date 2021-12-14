ff96693bddb5b484fe09da30e8858ce9b6d77f3f
Caltain <Caltain@abv.bg>
2021-12-14 19:36:43 +0200

    searchbar implemented

--- src/components/FurnitureList/FurnitureList.js
+++ src/components/FurnitureList/FurnitureList.js
@@ -1,12 +1,12 @@
 import { useEffect,useState } from "react";
 import FurnitureCard from "./FurnitureCard";
 import * as furnitureService from '../../services/furnitureService.js'
-import { CardGroup} from "react-bootstrap";
+import { CardGroup,Form,Row,Col,Button} from "react-bootstrap";
 
 
 const FurnitureList = () =>{
     const [furniture, setFurniture] = useState([]);
-
+    const [searchTerm, setSearchTerm] = useState('')
     useEffect(()=>{
             furnitureService.getAll()
             .then(result=>{
@@ -14,16 +14,30 @@ const FurnitureList = () =>{
             })
 
     },[])
-       
+      
     return (
             <>
             
             {furniture?.length > 0 
             ? (
-                <ul className="ulFurniture">
+                <ul className="ulFurniture" style={{margin:'auto'}}>
                     <h1>Furniture listings</h1>
+             <Form onChange={(e)=>{setSearchTerm(e.target.value)}} style={{margin:"auto", width:"19%"}}>
+                 <Row className="align-items-center">
+                     <Col sm={13.5} className="my-1">
+                      
+                       <Form.Control id="inlineFormInputName" placeholder="Search..." />
+                     </Col>
+                 </Row>
+            </Form>
                     <CardGroup>
-                    {furniture.map(x=><FurnitureCard key={x._id} furniturePiece={x} />)}
+                    {furniture.filter((x)=>{
+           if(searchTerm==""){
+               return x
+           }else if(x.name.toLowerCase().includes(searchTerm.toLowerCase())){
+               return x
+           }
+                  }).map(x=><FurnitureCard key={x._id} furniturePiece={x} />)}
                     </CardGroup>
                 </ul>
             )
